8318feda55209d141d54b61f29b448833be313dc
root <taekg.park@gmail.com>
2022-06-16 15:48:30 +0900

    init project

---
+++ src/main/kotlin/com/tkppp/tripleclubmileage/TripleClubMileageApplication.kt
@@ -0,0 +1,11 @@
+package com.tkppp.tripleclubmileage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TripleClubMileageApplication
+
+fun main(args: Array<String>) {
+    runApplication<TripleClubMileageApplication>(*args)
+}
