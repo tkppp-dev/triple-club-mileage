93461504373f08ad1ed166db07f5f5a734d115c8
root <taekg.park@gmail.com>
2022-06-17 16:29:33 +0900

    MileageController 테스트 코드 작성

--- src/main/kotlin/com/tkppp/tripleclubmileage/mileage/controller/MileageController.kt
+++ src/main/kotlin/com/tkppp/tripleclubmileage/mileage/controller/MileageController.kt
@@ -4,17 +4,19 @@ import com.tkppp.tripleclubmileage.mileage.dto.MileageLogResponseDto
 import com.tkppp.tripleclubmileage.mileage.dto.MileageSaveRequestDto
 import com.tkppp.tripleclubmileage.mileage.service.MileageService
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
+
 class MileageController(
     private val mileageService: MileageService
 ) {
 
     @PostMapping("/events")
-    fun mileageSaveEvents(@RequestBody mileageSaveRequestDto: MileageSaveRequestDto): ResponseEntity<String>{
+    fun getMileageSaveEvents(@RequestBody mileageSaveRequestDto: MileageSaveRequestDto): ResponseEntity<String>{
         mileageService.saveMileagePoint(mileageSaveRequestDto)
         val message = "MILEAGE_${mileageSaveRequestDto.action.fullname}_SUCCESS"
         return ResponseEntity(message, HttpStatus.OK)
